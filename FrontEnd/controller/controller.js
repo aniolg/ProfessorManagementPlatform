7f495e35ce6210242ba2f2b9e27c715fc88b8c54
aniolg <aniol_95@hotmail.com>
2019-01-29 13:51:21 +0100

    Remove list date

--- FrontEnd/controller/controller.js
+++ FrontEnd/controller/controller.js
@@ -5,12 +5,21 @@ $("document").ready(function () {
             addListDate(date);
         }
     });
-    $("#dates_list li i");
+    $("#dates_list").on("click", "li", function () {
+        removeListDate($("li").index(this));
+    });
+    $("#dates_list").on("mouseover mouseleave", "li", function () {
+        $(this).toggleClass("list-group-item-danger");
+    });
 });
 /*
 function addUnableDate(dates: string[]) {
     Professor.
 }*/
 function addListDate(date) {
-    $("#dates_list").append("<li class=\"list-group-item list-group-item-primary\">\n    <span>" + date + "</span>\n    <i class=\"fas fa-times-circle\"></i></li>");
+    $("#dates_list").append("<li class=\"list-group-item list-group-item-action list-group-item-primary\">\n    <span>" + date + "</span>\n    <i class=\"fas fa-times-circle\"></i></li>");
+}
+function removeListDate(indexLi) {
+    var numChild = indexLi + 1;
+    $("#dates_list li:nth-child(" + numChild + ")").remove();
 }
--- FrontEnd/controller/controller.ts
+++ FrontEnd/controller/controller.ts
@@ -1,12 +1,22 @@
 $("document").ready(function() {
+  
   $("#datepicker").datepicker({
     onSelect: function(date: string) {
       addListDate(date);
     }
   });
 
-  $("#dates_list li i")
+   $("#dates_list").on("click", "li", function () {
+        removeListDate($("li").index(this));
+    });
+  
+    $("#dates_list").on("mouseover mouseleave", "li", function(){
+      $(this).toggleClass("list-group-item-danger");
+    })
+
 });
+
+
 
 /*
 function addUnableDate(dates: string[]) {
@@ -14,7 +24,12 @@ function addUnableDate(dates: string[]) {
 }*/
 
 function addListDate(date: string) {
-    $("#dates_list").append(`<li class="list-group-item list-group-item-primary">
+    $("#dates_list").append(`<li class="list-group-item list-group-item-action list-group-item-primary">
     <span>${date}</span>
     <i class="fas fa-times-circle"></i></li>`);
+}
+
+function removeListDate(indexLi:number) {
+  let numChild = indexLi +1;
+  $("#dates_list li:nth-child(" + numChild + ")").remove();
 }
